4e9191470694b94b22fae0d89922593ab1db9d20
CaiquePrado <caiqueprado76@gmail.com>
2024-05-10 13:21:26 -0300

    chore: add initial project setup with dependencies

---
+++ src/main/java/dbserver/desafioviacep/desafioviacep/DesafioviacepApplication.java
@@ -0,0 +1,13 @@
+package dbserver.desafioviacep.desafioviacep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioviacepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioviacepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dbserver/desafioviacep/desafioviacep/DesafioviacepApplicationTests.java
@@ -0,0 +1,13 @@
+package dbserver.desafioviacep.desafioviacep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioviacepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
